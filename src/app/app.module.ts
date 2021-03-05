15203002b2004ddd7277b526e5798cab713a06bc
Ivan Katjounis <ivk.msg+github@gmail.com>
2021-03-05 14:48:31 +0100

    create c01 component (ng generate component c01)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { C01Component } from './c01/c01.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    C01Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/c01/c01.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { C01Component } from './c01.component';
+
+describe('C01Component', () => {
+  let component: C01Component;
+  let fixture: ComponentFixture<C01Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ C01Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(C01Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
