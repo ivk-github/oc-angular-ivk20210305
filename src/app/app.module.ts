ecb3f914b9df821ba1d538a1ce7ae8e4d03cf13f
Ivan Katjounis <ivk.msg+github@gmail.com>
2021-03-05 15:22:07 +0100

    create appareil component (ng g c appareil)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { C01Component } from './c01/c01.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    C01Component
+    C01Component,
+    AppareilComponent
   ],
   imports: [
     BrowserModule,
